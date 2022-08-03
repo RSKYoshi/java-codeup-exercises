98c7a419a6a65c7c05712057855685fe280dc214
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-08-03 15:40:55 -0500

    Bob program done

---
+++ src/Bob.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Bob {
+    public static void main(String[] args) {
+        boolean keepTalking;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.printf("%nAsk me a question:");
+
+            sc.useDelimiter("\n");
+            String userInput = sc.next();
+            if(userInput.contains("?")){
+                System.out.print("Bob: Sure");
+            } else if(userInput.contains("!")){
+                System.out.print("Bob: Whoa, chill out!");
+            } else if(userInput.length()==0){
+                System.out.print("Bob: Fine. Be that way!");
+            } else if(userInput.contains("stop")){
+                break;
+            } else System.out.print("Bob: Whatever");
+            String userEnter = sc.next();
+            keepTalking = userEnter.equals("stop");
+        }while(!keepTalking);
+    }
+}
---
+++ src/StringExercise.java
@@ -0,0 +1,19 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String pF = "We don't need no education\n" +
+                    "We don't need no thought control";
+        System.out.println(pF);
+
+        String w = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(w);
+
+        String m = "In windows, the main drive is usually C:\\";
+        System.out.println(m);
+
+        String b = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(b);
+
+
+    }
+}
